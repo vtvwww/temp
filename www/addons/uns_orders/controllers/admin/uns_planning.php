dd9a56967cc736592bdda0f189a83b796849e933
supervisor <vtv.www@gmail.com>
2014-04-23 16:00:42 +0300

    Подготовка данных для планирования

--- www/addons/uns_orders/controllers/admin/uns_planning.php
+++ www/addons/uns_orders/controllers/admin/uns_planning.php
@@ -6,22 +6,101 @@ if(!defined('AREA')){
 
 fn_uns_defaul_functions($controller, $mode);
 
+$pl = new planning();
+
 if($_SERVER['REQUEST_METHOD'] == 'POST'){
 }
-$pl = new planning();
 
 //**************************************************************************
 // РАСЧЕТЫ ПЛАНИРОВАНИЯ
 //**************************************************************************
 
 if ($mode == "calc"){
-    fn_print_r("получить список месяцев за текущий и предыдущий года");
-    $year_months = $pl->get_last_months(TIME);
+    if (!is__more_0($_REQUEST["month"], $_REQUEST["year"], $_REQUEST["week_supply"], $_REQUEST["years_for_analysis"])){
+        fn_set_notification("E", "Ошибка", "Правильно укажите исходные данные");
+    }
 
-    // Получить Фактические продажи насосов по каждому месяцу
-    $sales = $pl->get_sales($year_months);
+    $ps_id = array(29,37,77,76,65,66); // только К8/18 --- К290/30
 
-    fn_print_r($sales);
+    // 1. Параметры для расчета плана производства
+    $params = array(
+        "ps_id"             => $ps_id,
+        "month"             => $_REQUEST["month"],
+        "year"              => $_REQUEST["year"],
+        "week_supply"       => $_REQUEST["week_supply"],
+        "years_for_analysis"=> $_REQUEST["years_for_analysis"],
+    );
+
+    // 2. Расчет плана производства
+    $plan = $pl->calc($params);
+    $view->assign('plan', $plan);
+
+    // Серии насосов
+    list($pump_series) = fn_uns__get_pump_series(array('only_active' => true,));
+    $view->assign('pump_series', $pump_series);
+
+
+    $view->assign('search', $_REQUEST);
+
+    /*
+    //    fn_print_r("получить список месяцев за текущий и предыдущий года");
+        $year_months = $pl->get_last_months(TIME);
+
+        // Получить Фактические продажи насосов по каждому месяцу
+    //    $sales = $pl->get_sales($year_months);
+
+        $char_config = array("dir_charts"=>DIR_ROOT."/var/charts/");
+        $chart = new uns_chart($char_config);
+        $chart_data = array(
+            array(
+                "Янв"=>12,
+                "Фев"=>12,
+                "Мар"=>13,
+                "Май"=>13,
+                "Апр"=>13,
+                "Июн"=>13,
+                "Июл"=>13,
+                "Авг"=>13,
+                "Сен"=>13,
+                "Окт"=>13,
+                "Ноя"=>13,
+                "Дек"=>13,
+            ),
+        );
+
+        $chart->get_chart($chart_data);
+
+    //    fn_print_r($sales);
+
+
+        $data = array(
+            2013 => array(
+                "months" => array(
+                    1=> array(
+                        "pump_series" => array(
+                            29 => array(
+                                "quantity"=>6,
+                            ),
+                        ),
+                    ),
+                    2=> array(),
+                    3=> array(),
+                    4=> array(),
+                    5=> array(),
+                    6=> array(),
+                    7=> array(),
+                    8=> array(),
+                    9=> array(),
+                   10=> array(),
+                   11=> array(),
+                   12=> array(),
+                ),
+            ),
+
+        );
+
+
+        $view->assign('planning', $data);*/
 
 
 }
@@ -44,8 +123,10 @@ if ($mode == "calc"){
 
 function fn_uns_planning__search ($controller){
     $params = array(
-        'month',
-        'year',
+        "month",
+        "year",
+        "week_supply",
+        "years_for_analysis",
     );
     fn_uns_search_set_get_params($controller, $params);
     return true;
--- www/addons/uns_orders/core/fn.uns.planning.php
+++ www/addons/uns_orders/core/fn.uns.planning.php
@@ -6,13 +6,188 @@
  */
 class planning {
 
+    /**
+     * Расчет плана производства
+     * @param $params
+     */
+    public function calc ($params){
+        $years_for_analysis = $params["years_for_analysis"];
+        $ref_month          = $params["month"];
+        $ref_year           = $params["year"];
+        $week_supply        = $params["week_supply"];
+        $ps_id              = $params["ps_id"];
+
+        // 1. Получить список серий насосов
+        $pump_series = array_shift(fn_uns__get_pump_series(array("ps_id"=> $ps_id)));
+//        fn_print_r($pump_series);
+
+        // 2. Получить статистику продаж по указанным сериям за последние YEARS_FOR_ANALYSIS
+        $sales = self::get_sales(array_keys($pump_series), $years_for_analysis, $ref_month, $ref_year);
+        fn_print_r($sales);
+
+        // 3. Произвести анализ по каждой серии
+        $analysis = self::analysis_sales($sales, $ref_month, $ref_year);
+        fn_print_r($analysis);
+
+        // 4. Выполнить постороение графиков
+        $graphs = self::create_graphs($sales, $analysis);
+
+    }
+
+
+    /**
+     * Выполнить постороение графиков
+     * @param $sales
+     * @param $analysis
+     */
+    private function create_graphs ($sales, $analysis){
+        // 1. Выполнить подключение библиотеки
+
+    }
+
+    /**
+     * Выполнить анализ продаж
+     * @param $sales
+     * @param $ref_month
+     * @param $ref_year
+     * @return array|null
+     */
+    private function analysis_sales ($sales, $ref_month, $ref_year){
+        if (!is__array($sales)) return null;
+
+        $analysis = array();
+        foreach ($sales as $ps_id => $ps){
+            foreach ($ps as $k_y=>$year){
+                $for_year = array();
+                $for_months_ref_year = array();
+
+                // С начала года -------------------------------------------
+                $total_for_months_ref_year = 0;
+                for ($m=1; $m<$ref_month; $m++){
+                    $total_for_months_ref_year += $year[$m];
+                }
+
+                $avr_for_months_ref_year = ($ref_month==1)?0:$total_for_months_ref_year/($ref_month-1);
+                $for_months_ref_year = array(
+                    "total" => $total_for_months_ref_year,
+                    "avr"   => $avr_for_months_ref_year,
+                    "amount_months" => $ref_month-1,
+                );
+
+                // За год --------------------------------------------------
+                $for_year = array(
+                    "total" => ($k_y==$ref_year)?$avr_for_months_ref_year*12:array_sum($year),
+                    "avr"   => ($k_y==$ref_year)?$avr_for_months_ref_year:array_sum($year)/count($year),
+                );
+
+                // Результаты --------------------------------------------------
+                $analysis[$ps_id][$k_y]["for_year"] = $for_year;
+                $analysis[$ps_id][$k_y]["for_months_ref_year"] = $for_months_ref_year;
+            }
+        }
+        return $analysis;
+    }
+
+
+    /**
+     * Получить статистику продаж по указанным сериям за последние YEARS_FOR_ANALYSIS
+     * @param $ps_id
+     * @param $years_for_analysis
+     * @param $ref_month
+     * @param $ref_year
+     */
+    private function get_sales ($ps_id, $years_for_analysis, $ref_month, $ref_year){
+        $sales = false;
+        // 1. Получить список месячных интервалов
+        $months = self::get_months($years_for_analysis, $ref_year);
+//        fn_print_r($months);
+
+        // 2. Cобрать помесячные продажи
+        foreach ($months as $k_y=>$year){
+            foreach ($year as $k_m=>$month){
+                $balance = self::get_sales_pump_series_by_period($ps_id, $month["timestamp"]["begin"], $month["timestamp"]["end"]);
+                foreach ($ps_id as $ps){
+                    $sales[$ps][$k_y][$k_m] = $balance[$ps]?:0;
+                }
+            }
+        }
+        return $sales;
+    }
+
+    private function get_sales_pump_series_by_period ($ps_id, $begin, $end){
+        //todo необходимо оптимизировать функцию!!! 2014-04-23
+        list($pumps) = fn_uns__get_pumps(array("only_active"=>true, ));
+        $params = array(
+            "time_from"     => $begin,
+            "time_to"       => $end,
+            "type"          => 7,           // Расходные ордера
+            "o_id"          => 19,          // Склад готовой продукции
+            "only_active"   => true,
+            "item_type"     => array("P", "PF", "PA"),
+            "with_items"    => true,
+            "info_category" => false,
+            "info_item"     => true,
+            "info_unit"     => false,
+        );
+        list($docs) = fn_uns__get_documents($params);
+        if(!is__array($docs)) return null;
+
+        $res = array();
+        foreach ($docs as $d){
+            if (is__array($d["items"])){
+                foreach ($d["items"] as $i){
+                    if (in_array($pumps[$i["item_id"]]["ps_id"], $ps_id)){
+                        $res[$pumps[$i["item_id"]]["ps_id"]] += $i["quantity"];
+                    }
+                }
+            }
+        }
+        return $res;
+    }
+
 
     /**
      * Получить список отчетных месяцев
      * @param $time
      * @return array
      */
-    public function get_last_months ($time){
+    public function get_months($years_for_analysis, $ref_year){
+        $res = array();
+        for ($year=$ref_year-$years_for_analysis+1; $year<=$ref_year; $year++){
+            for ($month=1; $month<=12; $month++){
+                $begin = $year . "-" . $month . "-" . "1" . " 00:00:00";
+                $end = $year . "-" . $month . "-" . date("t", strtotime($begin))  . " 23:59:59";
+                $res[$year][$month]["date"]["begin"] = $begin;
+                $res[$year][$month]["date"]["end"]   = $end;
+                $res[$year][$month]["timestamp"]["begin"] = strtotime($begin);
+                $res[$year][$month]["timestamp"]["end"]   = strtotime($end);
+            }
+        }
+        return $res;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //==========================================================================
+    // OLD FUNCTIONS
+    //==========================================================================
+
+    /**
+     * Получить список отчетных месяцев
+     * @param $time
+     * @return array
+     */
+    public function get_last_months_old ($time){
         $curr_year  = date("Y", $time);
         $curr_month = date("Y", $time);
 
@@ -29,10 +204,10 @@ class planning {
     }
 
 
-    public function get_sales ($periods){
+    public function get_sales_old ($periods){
         foreach ($periods as $k_y=>$y){
             foreach ($y as $k_m=>$m){
-                $balance = self::get_sales_by_month ($m["date"]);
+                $balance = self::get_sales_by_month_old($m["date"]);
                 $periods[$k_y][$k_m]["balance"] = $balance;
                 $periods[$k_y][$k_m]["total_balance"] = array_sum($balance);
             }
@@ -40,7 +215,7 @@ class planning {
         return $periods;
     }
 
-    private function get_sales_by_month ($m){
+    private function get_sales_by_month_old ($m){
         list($pumps) = fn_uns__get_pumps(array("only_active"=>true, ));
         $params = array(
             "time_from"     => strtotime($m["begin"]),
